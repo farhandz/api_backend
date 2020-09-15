dc1bcc2fd59d04c3fca75ac7644381d799a2f2c3
farhandz <ammarjoz09@gmail.com>
2020-09-15 22:07:46 +0700

    first commit

--- connection/db.js
+++ connection/db.js
@@ -5,6 +5,7 @@ const conn = connection.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   database: process.env.DATABASE_NAME,
+  password: "Ammarpedia123@@@",
   dateStrings: "date"
 });
 
